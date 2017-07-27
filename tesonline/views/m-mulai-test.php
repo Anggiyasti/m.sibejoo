5a3d8519b08592ce2da7cb230d18c6dc270e48ee
Anggiyasti <putrianggiyasti@gmail.com>
2017-07-27 17:34:37 +0700

    Fix pesan di hosting

--- tesonline/views/m-mulai-test.php
+++ tesonline/views/m-mulai-test.php
@@ -22,47 +22,68 @@
             <div class="page page-bg">
                 <div class="page-content">
 
-                    <h3 class="text-center" style="color: white">Daftar Latihan <a onclick="latihan()" class="btn btn-gray btn-theme-colored hvr-icon-float-away">Buat Latihan&nbsp&nbsp</a></h3>
-                    <div class="row mb-30" style="background-color: #fff">
-                        <div class="col-100">
-                            <?php if ($latihan == array()): ?>
-                                <h4>Tidak ada latihan.</h4>
-                            <?php else: ?>
-                                <div class="content-block mt-0 mb-0">
-                                    <?php foreach ($latihan as $latihanitem): ?>
-                                        <blockquote>
-                                            Nama Latihan : <?= $latihanitem['nm_latihan'] ?> <br>
-                                            Level : <?= $latihanitem['tingkatKesulitan'] ?> <br>
-                                            Jumlah Soal : <?= $latihanitem['jumlahSoal'] ?> <br>
-                                            Tanggal Pengerjaan : <?= $latihanitem['date_created'] ?> <br>
-                                            <a class="button button-smaall js-form-submit button-fill button-success detail-<?= $latihanitem['id_latihan'] ?>" title="Kerjakan" onclick="mulai_test(<?= $latihanitem['id_latihan'] ?>)"><i class="fa fa-pencil"></i> Kerjakan</a>
-                                        </blockquote>
-                                    <?php endforeach ?>  
-                                </div>
-                            <?php endif ?>
+                    <div class="content-block mb-15">
+                        <div class="buttons-row">
+                            <a href="#tab1-1" class="tab-link active button button-primary">Daftar Latihan</a>
+                            <a href="#tab2-1" class="tab-link button button-primary">Daftar Report</a>
                         </div>
                     </div>
 
-                    <h3 class="text-center" style="color: white">Daftar Report</h3>
-                    <div class="row mb-30" style="background-color: #fff">
-                        <div class="col-100">
-                            <?php if ($report == array()): ?>
-                                <h4>Tidak ada Report Latihan.</h4>
-                            <?php else: ?>
-                                <div class="content-block mt-0 mb-0">
-                                    <?php foreach ($report as $reportitem): ?>
-                                        <blockquote>
-                                            Nama Latihan : <?= $reportitem['nm_latihan'] ?> <br>
-                                            Level : <?= $reportitem['tingkatKesulitan'] ?> <br>
-                                            Tanggal Pengerjaan : <?= $reportitem['tgl_pengerjaan'] ?> <br>
-                                            <a class="button button-smaall js-form-submit button-fill button-success modal-on<?= $reportitem['id_latihan'] ?>" title="Lihat score" onclick="lihat_score(<?= $reportitem['id_latihan'] ?>)" data-todo='<?= json_encode($reportitem) ?>' style="margin-bottom: 5px;"><i class="fa fa-book"></i> Score</a>
-                                            <a class="button button-smaall js-form-submit button-fill button-success modal-on<?= $reportitem['id_latihan'] ?>" onclick="mulai_pembahasan(<?= $reportitem['id_latihan'] ?>)" data-todo='<?= json_encode($reportitem) ?>'><i class="fa fa-book"></i> Pembahasan</a>
-                                        </blockquote>
-                                    <?php endforeach ?>  
+                    <div class="tabs">
+                        <!-- tab-pane: latihan -->
+                        <div id="tab1-1" class="tab active">
+                            <div class="content-block mt-15">
+                                <a onclick="latihan()" class="button button-small js-form-submit button-fill button-success">Buat Latihan&nbsp&nbsp</a> <br>
+                                <div class="row mb-30" style="background-color: #fff">
+                                    <div class="col-100">
+                                        <?php if ($latihan == array()): ?>
+                                            <h4>Tidak ada latihan.</h4>
+                                        <?php else: ?>
+                                            <div class="content-block mt-0 mb-0">
+                                                <?php foreach ($latihan as $latihanitem): ?>
+                                                    <blockquote>
+                                                        Nama Latihan : <?= $latihanitem['nm_latihan'] ?> <br>
+                                                        Level : <?= $latihanitem['tingkatKesulitan'] ?> <br>
+                                                        Jumlah Soal : <?= $latihanitem['jumlahSoal'] ?> <br>
+                                                        Tanggal Pengerjaan : <?= $latihanitem['date_created'] ?> <br>
+                                                        <a class="button button-small js-form-submit button-fill button-success detail-<?= $latihanitem['id_latihan'] ?>" title="Kerjakan" onclick="mulai_test(<?= $latihanitem['id_latihan'] ?>)"><i class="fa fa-pencil"></i> Kerjakan</a>
+                                                    </blockquote>
+                                                <?php endforeach ?>  
+                                            </div>
+                                        <?php endif ?>
+                                    </div>
                                 </div>
-                            <?php endif ?>
+                            </div>
+                            <!-- /tab-pane: latihan -->
                         </div>
+                        <!-- tab-pane: report -->
+                        <div id="tab2-1" class="tab">
+                            <div class="content-block mt-15">
+                                <h3 class="text-center" style="color: white">Daftar Report</h3>
+                                <div class="row mb-30" style="background-color: #fff">
+                                    <div class="col-100">
+                                        <?php if ($report == array()): ?>
+                                            <h4>Tidak ada Report Latihan.</h4>
+                                        <?php else: ?>
+                                            <div class="content-block mt-0 mb-0">
+                                                <?php foreach ($report as $reportitem): ?>
+                                                    <blockquote>
+                                                        Nama Latihan : <?= $reportitem['nm_latihan'] ?> <br>
+                                                        Level : <?= $reportitem['tingkatKesulitan'] ?> <br>
+                                                        Tanggal Pengerjaan : <?= $reportitem['tgl_pengerjaan'] ?> <br>
+                                                        <a class="button button-smaall js-form-submit button-fill button-success modal-on<?= $reportitem['id_latihan'] ?>" title="Lihat score" onclick="lihat_score(<?= $reportitem['id_latihan'] ?>)" data-todo='<?= json_encode($reportitem) ?>' style="margin-bottom: 5px;"><i class="fa fa-book"></i> Score</a>
+                                                        <a class="button button-smaall js-form-submit button-fill button-success modal-on<?= $reportitem['id_latihan'] ?>" onclick="mulai_pembahasan(<?= $reportitem['id_latihan'] ?>)" data-todo='<?= json_encode($reportitem) ?>'><i class="fa fa-book"></i> Pembahasan</a>
+                                                    </blockquote>
+                                                <?php endforeach ?>  
+                                            </div>
+                                        <?php endif ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!--/ tab-pane: report -->
                     </div>
+
     
                 </div>
             </div>
